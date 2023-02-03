0379f4353f5e60ca2381f68ee85262df610a2fcf
jimdeal <jimdeal@hotmail.com>
2023-02-03 09:21:33 +0000

    add in some comments - some base settings are not specified to cat variants - and as such some base settings may not make sense for some cat variants
    we can only test against what "requirements" we can see!!

--- src/main/java/com/techreturners/cats/CatImpl.java
+++ src/main/java/com/techreturners/cats/CatImpl.java
@@ -6,12 +6,16 @@ public abstract class CatImpl implements Cat {
     private final String AWAKE_MESSAGE = "Cat should be awake now";
     private final String GO_TO_SLEEP_MESSAGE = "Cat should be snoozing";
     public final String CAT_SETTING = "cat";
+
     // is this correct - should we initialise eat to a noise we may be unsure of ?
     public final String CAT_EAT = "mewww";
 
-    // who knows the height of "base cat" - so make nonsense?
+    // who knows the average height of "base cat" - so make nonsense?
+    // who knows the average height of other cats - unless specified
     public final int NONSENSE_AVERAGE_HEIGHT = -1;
 
+    // This is private - as it's not changed anywhere else
+    // would need to be protected if this is different from subclasses
     private String asleepMessage = ASLEEP_MESSAGE;
     protected String catSetting = CAT_SETTING;
     protected int averageHeight = NONSENSE_AVERAGE_HEIGHT;
