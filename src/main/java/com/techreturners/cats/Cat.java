bceee4d1d6e51b6f6e9f4555d8bfe4f72b570d13
jimdeal <jimdeal@hotmail.com>
2023-02-03 08:41:09 +0000

    sort out final strings for asleep message
    add in prototype of getAverageHeight
    WIP

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -5,10 +5,9 @@ interface Cat {
     String isAsleep();
     void goToSleep();
     void wakeUp();
-
     String getSetting();
-    /*
     int getAverageHeight();
+    /*
     String eat();
  */
 
--- src/main/java/com/techreturners/cats/CatImpl.java
+++ src/main/java/com/techreturners/cats/CatImpl.java
@@ -2,9 +2,11 @@ package com.techreturners.cats;
 
 public abstract class CatImpl implements Cat {
 
-    private String asleepMessage = "Cat should be awake by default";
-    private String awakeMessage = "Cat should be awake now";
-    private String goToSleepMessage = "Cat should be snoozing";
+    private final String ASLEEP_MESSAGE = "Cat should be awake by default";
+    private final String AWAKE_MESSAGE = "Cat should be awake now";
+    private final String GO_TO_SLEEP_MESSAGE = "Cat should be snoozing";
+    private String asleepMessage = ASLEEP_MESSAGE;
+
     public final String CAT_SETTING = "cat";
     protected String catSetting = CAT_SETTING;
 
@@ -12,13 +14,18 @@ public abstract class CatImpl implements Cat {
         return asleepMessage;
     }
     public void goToSleep(){
-        asleepMessage = goToSleepMessage;
+        asleepMessage = GO_TO_SLEEP_MESSAGE;
     }
     public void wakeUp(){
-        asleepMessage = awakeMessage;
+        asleepMessage = AWAKE_MESSAGE;
     }
     public String getSetting(){
         return catSetting;
     }
+
+    public int getAverageHeight(){
+        return 0;
+    }
+
 
 }
